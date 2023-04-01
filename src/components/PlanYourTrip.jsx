f8cee365de1540b2bb5f1fd7dcbf0a12384e35b1
usepsick <usepsick@gmail.com>
2023-04-01 18:29:40 +0200

    plan your trip

---
+++ src/components/PlanYourTrip.jsx
@@ -0,0 +1,13 @@
+import './PlanYourTrip.css'
+
+function PlanYourTrip () {
+  return (
+    <>
+      <div className='plan-trip'>
+        <h1>Plan your trip</h1>
+      </div>
+    </>
+  )
+}
+
+export default PlanYourTrip
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from '../components/Banner'
 import BookCar from '../components/BookCar'
+import PlanYourTrip from '../components/PlanYourTrip'
 
 function Home () {
   return (
     <>
       <Banner />
       <BookCar />
+      <PlanYourTrip />
     </>
   )
 }
